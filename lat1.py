fbb94146538adeedc350f4be431d28e50a3c4ff0
puput <putri48822@gmail.com>
2024-03-04 14:20:45 +0900

    r1

---
+++ lat1.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import pandas as pd
+#import matplotlib.pyplot as pyplot
+
+st.write("Infinity Coding Club")
+df = pd.DataFrame({
+    'No': [1,2,3,4],
+    'Nama': ['Anita','Putri','Kiya','Ety'],
+    'Nilai': [100,100,100,100]
+    })
+
+df
