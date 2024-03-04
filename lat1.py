a509bdf2d87c1bcc1ef2e024505ee69d4bede8e7
puput <putri48822@gmail.com>
2024-03-04 14:32:25 +0900

    r2

--- lat1.py
+++ lat1.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 #import matplotlib.pyplot as pyplot
 
-st.write("Infinity Coding Club")
+st.write("Tabel Coding Puput")
 df = pd.DataFrame({
     'No': [1,2,3,4],
     'Nama': ['Anita','Putri','Kiya','Ety'],
